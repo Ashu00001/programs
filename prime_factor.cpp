145d72a45c5123a5ad762f112e9fd9e11d92fb2c
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-08-24 21:26:36 +0530

    Update prime_factor.cpp

--- prime_factor.cpp
+++ prime_factor.cpp
@@ -1,21 +1,46 @@
 #include<iostream>
 using namespace std;
+int a[100];
+void prime(int a[],int j)
+{
+	int i=2;
+	for(int i=0;i<j;i++)
+		{
+		int k=1;
+		int count=0;
+		while(k<a[i])
+		{
+			if(a[i]%k==0)
+			{
+				count++;
+			}
+			k++;
+		}
+		if(count<=1)
+		{
+			cout<<a[i]<<"\t";
+		}
+}
+}
+void factor(int x)
+{
+	int i=2;
+	int j=0;
+	while(i<=x)
+	{
+		if(x%i==0)
+		{
+			a[j]=i;
+			j++;
+		}
+		i++;
+	}
+
+	prime(a,j);
+}
 int main()
 {
-	int a,countfor=0,countwhile=0;
-	cin>>a;
-	for(int i=2;i<a;i++)
-{
-		if(a%i==0){
-	while(a%i==0)
-	{
-		cout<<i<<"\n";
-		a=a/i;
-		countwhile++;
-	}
-					}
-					countfor++;
-
-}
-cout<<"\n\n"<<countfor<<endl<<countwhile<<"\n\n"<<endl;
+	int x;
+	cin>>x;
+	factor(x);
 }
